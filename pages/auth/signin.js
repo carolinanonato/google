c82eec391a97d9fb12747c61baaedd626a658502
Carolina Nonato <csnonato@gmail.com>
2022-03-16 09:30:55 -0400

    auth - 2nd part done

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import Header from '../../components/Header'
 
-export default function signin() {
+import { getProviders } from "next-auth/react"
+
+
+//client side
+export default function signin({ providers }) {
     return (
-        <div>signin</div>
+        <div>
+            <Header />
+
+            <div className="">
+                {Object.values(providers).map(provider => (
+                    <div key={provider.name} className="">
+                        <img src="https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google logo" />
+                        <p>This website is created for learning purposes</p>
+                        <button>Sign in with {provider.name}</button>
+                    </div>
+                ))}
+
+            </div>
+        </div>
     )
+}
+
+//server side
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return {
+        props: { providers }
+    }
 }

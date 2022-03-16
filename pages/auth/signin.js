714b758c46120fc56ffd27ea37068404f653e981
Carolina Nonato <csnonato@gmail.com>
2022-03-16 15:29:56 -0400

    add auth and get user data from google

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../components/Header'
-
-import { getProviders } from "next-auth/react"
+import { getProviders, signIn } from "next-auth/react"
 
 
 //client side
@@ -10,12 +9,12 @@ export default function signin({ providers }) {
         <div>
             <Header />
 
-            <div className="">
+            <div className="mt-32">
                 {Object.values(providers).map(provider => (
-                    <div key={provider.name} className="">
-                        <img src="https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google logo" />
-                        <p>This website is created for learning purposes</p>
-                        <button>Sign in with {provider.name}</button>
+                    <div key={provider.name} className="flex flex-col items-center">
+                        <img className="w-80 object-cover" src="https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google logo" />
+                        <p className='text-sm italic my-10 mt-0 text-center'>This website is created for learning purposes</p>
+                        <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={() => signIn(provider.id, { callbackUrl: "/" })}>Sign in with {provider.name}</button>
                     </div>
                 ))}
 

a9d7dd413f0738ecfd1af0240c409e598663a894
Carolina Nonato <csnonato@gmail.com>
2022-03-17 18:50:03 -0400

    Create the body section

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -12,7 +12,7 @@ export default function signin({ providers }) {
             <div className="mt-32">
                 {Object.values(providers).map(provider => (
                     <div key={provider.name} className="flex flex-col items-center">
-                        <img className="w-80 object-cover" src="https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google logo" />
+                        <img className="w-52 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" alt="google logo" />
                         <p className='text-sm italic my-10 mt-0 text-center'>This website is created for learning purposes</p>
                         <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={() => signIn(provider.id, { callbackUrl: "/" })}>Sign in with {provider.name}</button>
                     </div>

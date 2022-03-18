580cb88e1c20e18f36e135fb3893264ad39afc83
Carolina Nonato <csnonato@gmail.com>
2022-03-18 10:46:12 -0400

    search header component

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import Image from "next/image"
 import { useRouter } from 'next/router'
+import { useRef } from 'react'
+import search from '../pages/search'
+import { MicrophoneIcon, SearchIcon, XIcon } from '@heroicons/react/solid'
+import User from './User'
 
 
 export default function SearchHeader() {
     const router = useRouter()
+    const searchInputRef = useRef(null)
+
+    function search(event) {
+        event.preventDefault();
+        const term = searchInputRef.current.value
+        if (!term.trim()) return
+        router.push(`/search?term=${term.trim()}`)
+    }
+
+
     return (
         <header className='sticky top-0 bg-white'>
-            <div className="">
+            <div className="flex w-full p-6 items-center">
                 <Image
                     onClick={() => router.push("/")}
                     width="120"
@@ -16,6 +30,24 @@ export default function SearchHeader() {
                     className='cursor-pointer'
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
                     alt="google logo" />
+
+                <form className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center'>
+                    <input
+                        type="text"
+                        defaultValue={router.query.term}
+                        ref={searchInputRef}
+                        className='w-full focus:outline-none' />
+
+                    <XIcon
+                        className='h-7 text-gray-500 cursor-pointer sm:mr-3'
+                        onClick={() => (searchInputRef.current.value = "")} />
+                    <MicrophoneIcon className='h-6 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3' />
+                    <SearchIcon className='h-6 hidden sm:inline-flex text-blue-500' />
+                    <button onClick={search} type='submit' hidden></button>
+
+                </form>
+
+                <User className="ml-auto whitespace-nowrap" />
             </div>
 
         </header>
